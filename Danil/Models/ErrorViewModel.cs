95d97821bf98adb0fa963f31f493183df550a7c6
HP <HP@ROTTSON>
2024-01-04 12:25:07 +0100

    Add project files.

---
+++ Danil/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Danil.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
